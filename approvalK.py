c164f235c4442cd45d7e49f2e9198773c21c275d
Ryan Yang <y.ryan.yang@gmail.com>
2022-02-26 20:46:01 -0500

    approvalK voting is quite simple

--- approvalK.py
+++ approvalK.py
@@ -1,8 +1,21 @@
-n = 10
-k = 3
-
-voters = 100
+import numpy as np
 
 
-for i in range(voters):
-	pass
+def main():
+	n = 10
+	k = 1
+
+	voters = 5
+
+
+	candidates = [0 for i in range(n)]
+	for i in range(voters):
+		arr = np.random.permutation(n)
+		print(arr)
+		for j in range(k):
+			candidates[arr[j]]+=1
+
+	print(candidates)
+
+if __name__ == "__main__":
+	main()
